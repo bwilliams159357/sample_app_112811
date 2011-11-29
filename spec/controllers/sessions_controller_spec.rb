fb73a0853dc5976c7d137f800fc7d3f0e597af04
Brian Williams <bwilliam@cs.utsa.edu>
2011-11-29 10:03:55 -0600

    Added rspec tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe SessionsController do
+  render_views
 
   describe "GET 'new'" do
     it "returns http success" do
       get 'new'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'new'
+      respone.should have_selector("title", :content => "Sign in")
     end
   end
 
